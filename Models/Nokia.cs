b6f4643da21c9511fbe51125a85ce6276129d2de
Ptriick1 <110056527+Ptriick1@users.noreply.github.com>
2025-09-24 08:55:24 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // Herdando de Smartphone
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string cor) 
+            : base(numero, modelo, cor)
+        {
+        }
+
+        // Sobrescrevendo InstalarAplicativo
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} através da loja da Nokia...");
+        }
     }
 }
