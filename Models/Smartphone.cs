a3233d4f32745c1d20e62c21e3b39e7d4f81d2a8
Ptriick1 <110056527+Ptriick1@users.noreply.github.com>
2025-09-24 08:55:04 -0300

    Update Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,14 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string Cor { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string cor)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Cor = cor;
         }
 
         public void Ligar()
