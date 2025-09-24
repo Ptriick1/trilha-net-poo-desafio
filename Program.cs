57124ba1bb34e3614d8ecd0fdae7095e566206dc
Ptriick1 <110056527+Ptriick1@users.noreply.github.com>
2025-09-24 08:57:45 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone meuIphone = new Iphone("99999-0000", "iPhone 14", "Preto");
+Nokia meuNokia = new Nokia("88888-0000", "Nokia 3310", "Azul");
+
+meuIphone.Ligar();
+meuIphone.InstalarAplicativo("WhatsApp");
+
+meuNokia.Ligar();
+meuNokia.InstalarAplicativo("TeamfightTatics");
